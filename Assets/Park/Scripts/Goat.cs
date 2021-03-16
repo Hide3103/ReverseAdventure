175164cfaae21b9d18c4ac9579385cb075124126
grape_ming <dpqks1234567@gmail.com>
2021-03-16 15:52:05 +0900

    ヤギの動き関連のタスクはほとんど完了

---
+++ Assets/Park/Scripts/Goat.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goat : MonoBehaviour
+{
+    GameObject player;
+    GameObject lookPlayer;
+    GameObject lostPlayer;
+
+    Rigidbody2D rigid2D;
+
+    Vector2 firstScale;
+
+    SpriteRenderer spriteRenderer;
+
+    Color32 beforeColor;
+
+    bool flg_normal;
+    bool flg_lookPlayer;
+    bool flg_moveToPlayer;
+    bool flg_lostPlayer;
+    bool flg_danger;
+    bool flg_damage;
+
+    float m_speed;
+    float m_direction;
+    float m_moveToplayerTime;
+    float m_lostPlayerTime;
+    float m_DamageDelta = 0.0f;
+    float m_DamageLimit = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.player = GameObject.Find("Player");
+        this.lookPlayer = GameObject.Find("LookPlayer");
+        this.lostPlayer = GameObject.Find("LostPlayer");
+
+        this.rigid2D = GetComponent<Rigidbody2D>();
+
+        this.firstScale = transform.localScale;
+
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigid2D.constraints = RigidbodyConstraints2D.FreezeRotation; //ローテーション固定
+
+        Move();
+        MoveToPlayer();
+        Damage();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //"Sword"に当たった時にフラグ切り替え＆ノックバック
+        if (collision.gameObject.tag == "Sword")
+        {
+            flg_damage = true;
+            var rigidbody = GetComponent<Rigidbody2D>();
+       
+            rigidbody.AddForce(new Vector3(transform.localScale.x * 60.0f, 50.0f, 0.0f));
+        }
+    }
+
+    void Initialize()
+    {
+        flg_normal = true;
+        flg_lookPlayer = false;
+        flg_moveToPlayer = false;
+        flg_lostPlayer = false;
+        flg_danger = false;
+        flg_damage = false;
+
+        m_direction = -1.0f;
+        m_speed = 0.5f;
+        m_moveToplayerTime = 0.0f;
+        m_lostPlayerTime = 0.0f;
+    }
+
+    void Move()
+    {
+        Vector2 Position = transform.position;
+        Transform target = player.transform;
+        Vector2 direction = (target.position - transform.position).normalized;
+
+        float accelaration = 2.0f; //速度
+        float velocity = (accelaration * Time.deltaTime); //速度を秒速に変える
+        float distance = Vector2.Distance(target.position, transform.position);
+
+        if (flg_normal)
+        {
+            transform.localScale = new Vector3(firstScale.x, firstScale.y);
+
+            Position.x += m_speed * m_direction * Time.deltaTime;
+
+            transform.position = Position;
+
+            lookPlayer.SetActive(false);
+            lostPlayer.SetActive(false);
+        }
+
+        if (flg_lookPlayer)
+        {
+            Direction();
+
+            lookPlayer.SetActive(true);
+            lostPlayer.SetActive(false);
+
+            m_moveToplayerTime += Time.deltaTime;
+
+            if (m_moveToplayerTime >= 1.0f)
+            {
+                m_moveToplayerTime = 0.0f;
+
+                flg_moveToPlayer = true;
+                flg_lookPlayer = false;
+            }
+        }
+
+        if (flg_moveToPlayer)
+        {
+            if (player)
+            {
+                Direction();
+
+                this.transform.position = new Vector2(transform.position.x + (direction.x * velocity),
+                                          transform.position.y + (direction.y * velocity)); //追いかける
+            }
+        }
+
+        if (flg_lostPlayer)
+        {
+            lostPlayer.SetActive(true);
+            lookPlayer.SetActive(false);
+
+            m_lostPlayerTime += Time.deltaTime;
+
+            if (m_lostPlayerTime >= 5.0f)
+            {
+                m_lostPlayerTime = 0.0f;
+
+                flg_normal = true;
+                flg_lookPlayer = false;
+                flg_moveToPlayer = false;
+                flg_lostPlayer = false;
+                flg_danger = false;
+            }
+        }
+    }
+
+    void MoveToPlayer()
+    {
+        Transform target = player.transform;
+        float distance = Vector2.Distance(target.position, transform.position);
+
+        if (distance <= 7.0f)
+        {
+            flg_normal = false;
+            flg_lookPlayer = true;
+            flg_lostPlayer = false;
+            flg_danger = true;
+
+            m_lostPlayerTime = 0.0f;
+        }
+        else if(distance > 7.0f && flg_danger)
+        {
+            flg_lookPlayer = false;
+            flg_moveToPlayer = false;
+            flg_lostPlayer = true;
+
+            m_moveToplayerTime = 0.0f;
+        }
+    }
+
+    void Damage()
+    {
+        if (flg_damage)
+        {
+            if (m_DamageDelta < m_DamageLimit)
+            {
+                float level = Mathf.Abs(Mathf.Sin(Time.time * 20));
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, level);
+                m_DamageDelta += Time.deltaTime;
+            }
+            else
+            {
+                spriteRenderer.color = beforeColor;
+                flg_damage = false;
+                m_DamageDelta = 0.0f;
+            }
+        }
+    }
+
+    void Direction()
+    {
+        Vector2 scale = transform.localScale;
+        if (player.transform.position.x < this.transform.position.x)
+        {
+            transform.localScale = new Vector3(firstScale.x, firstScale.y);
+        }
+        else if (player.transform.position.x > this.transform.position.x)
+        {
+            transform.localScale = new Vector3(-firstScale.x, firstScale.y);
+        }
+    }
+}
