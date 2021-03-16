9cf43c672bba2c2fa8c2ba6d9d24ed6a3455dc96
Hide3103 <zeal19300216@c-league.jp>
2021-03-16 15:48:27 +0900

    ステージセレクト画面で、カーソルキーで横に画面がスライドするように変更

---
+++ Assets/Hideaki/Scripts/StageSelectImageScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelectImageScript : MonoBehaviour
+{
+    public int SelectingStageNum = 1;
+    int StageMaxNum = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SelectingStageNum < StageMaxNum)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.position += new Vector3(5.0f, 0.0f, 0.0f);
+                SelectingStageNum += 1;
+            }
+        }
+        if (1 < SelectingStageNum)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.position += new Vector3(-5.0f, 0.0f, 0.0f);
+                SelectingStageNum -= 1;
+            }
+        }
+    }
+}
