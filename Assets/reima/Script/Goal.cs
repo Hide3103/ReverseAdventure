ce8851a7573a41698b3095fd62d854ee074bc2f4
reima398 <reima398@gmail.com>
2021-03-21 06:49:36 +0900

    Goalスクリプトの追加 goalするとgoal地点にアップされるように

---
+++ Assets/reima/Script/Goal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public bool IsGoal = false;
+    public GameObject MainCamera;
+    public GameObject GoalCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GoalCamera.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsGoal)
+        {
+            GoalCamera.SetActive(true);
+            MainCamera.SetActive(false);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag=="Player")
+        {
+            IsGoal = true;
+        }
+    }
+}
