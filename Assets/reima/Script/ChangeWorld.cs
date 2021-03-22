9ccf5f6b0524473cf67b0e62d1ca9bf1d3a3e94c
reima398 <reima398@gmail.com>
2021-03-22 15:46:48 +0900

    簡単に裏表反転の実装

---
+++ Assets/reima/Script/ChangeWorld.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeWorld : MonoBehaviour
+{
+    public GameObject UraObj;
+    public GameObject OmoteObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            UraObj.SetActive(!UraObj.activeSelf);
+            OmoteObj.SetActive(!OmoteObj.activeSelf);
+        }
+    }
+}
