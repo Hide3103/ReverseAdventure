064e85bc87b3f721a6529785a8030de1c5c80742
reima398 <reima398@gmail.com>
2021-03-24 23:06:27 +0900

    ダイヤ取得のUI追加

---
+++ Assets/reima/Script/DiaUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiaUI : MonoBehaviour
+{
+    public Text DiaUIText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DiaUIText.text = ("× " + GameSystem.NumJewel);
+    }
+}
