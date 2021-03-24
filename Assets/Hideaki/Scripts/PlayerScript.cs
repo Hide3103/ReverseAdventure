f049a5d6566dafc337dc3d6e45aba36d89438c35
reima398 <reima398@gmail.com>
2021-03-24 14:09:29 +0900

    m_IsPlayがfalseになっていて動かないバグを修正

--- Assets/Hideaki/Scripts/PlayerScript.cs
+++ Assets/Hideaki/Scripts/PlayerScript.cs
@@ -22,7 +22,7 @@ public class PlayerScript : MonoBehaviour
 
     public  bool m_TurnFlg = false;
 
-    public static bool m_IsPlay = false;
+    public static bool m_IsPlay = true;
 
     //UIで管理するのにstaticにしました 3/20
     public static int m_PlayerHp = 3;
