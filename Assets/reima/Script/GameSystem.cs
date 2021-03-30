0be67cc2fee8d9d59f264903b895ab7b80f864c1
reima398 <reima398@gmail.com>
2021-03-30 15:33:40 +0900

    ステージごとの変数numjewelの追加とステージに応じたjewelの今までの最大取得数が見えるようになった

--- Assets/reima/Script/GameSystem.cs
+++ Assets/reima/Script/GameSystem.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class GameSystem : MonoBehaviour
 {
     public static bool IsGoal = false;
+    //クリアタイム
     public static float ClearTime = 0;
+    
     public static float NumJewel = 0;
+    //これまでで最大何個とったかを入れる変数
+    public static int NumMaxGetJewelStage1 = 0;
+    public static int NumMaxGetJewelStage2 = 0;
+    public static int NumMaxGetJewelStage3 = 0;
+    //今のゲームで何個とったかを入れる変数
+    public static int NumNowMaxGetJewelStage1 = 0;
+    public static int NumNowMaxGetJewelStage2 = 0;
+    public static int NumNowMaxGetJewelStage3 = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +29,18 @@ public class GameSystem : MonoBehaviour
         if(PlayerScript.m_IsPlay)
         {
             ClearTime += Time.deltaTime;
+        }
+        if(NumNowMaxGetJewelStage1>NumMaxGetJewelStage1)
+        {
+            NumMaxGetJewelStage1 = NumNowMaxGetJewelStage1;
+        }
+        if (NumNowMaxGetJewelStage2 > NumMaxGetJewelStage2)
+        {
+            NumMaxGetJewelStage2 = NumNowMaxGetJewelStage2;
+        }
+        if (NumNowMaxGetJewelStage3 > NumMaxGetJewelStage3)
+        {
+            NumMaxGetJewelStage3 = NumNowMaxGetJewelStage3;
         }
     }
 }
---
+++ Assets/reima/Script/GetDiaScoreUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetDiaScoreUI : MonoBehaviour
+{
+    //text
+    public Text NumGetMaxDiaText1;
+    public Text NumGetMaxDiaText2;
+    public Text NumGetMaxDiaText3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NumGetMaxDiaText1.text = "現在の最高取得数　：　"+GameSystem.NumMaxGetJewelStage1;
+        NumGetMaxDiaText2.text = "現在の最高取得数　：　" + GameSystem.NumMaxGetJewelStage2;
+        NumGetMaxDiaText3.text = "現在の最高取得数　：　" + GameSystem.NumMaxGetJewelStage3;
+    }
+}
