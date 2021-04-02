c4f59ffbef3c7a3744a54a28ef1fa2767b864b96
reima398 <reima398@gmail.com>
2021-04-02 13:29:01 +0900

    ゲームオーバーシーンの演出実装

---
+++ Assets/reima/Script/GameOverDirecting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverDirecting : MonoBehaviour
+{
+    public CanvasGroup TitleCanvasGroupstage1;
+    public CanvasGroup TitleCanvasGroupstage2;
+    float PlusTime = 0.5f;
+    float SetAlpha1 = 0.0f;
+    float SetAlpha2 = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TitleCanvasGroupstage1.alpha = 0;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SetAlpha1 < 1)
+        {
+            SetAlpha1 += PlusTime * Time.deltaTime;
+        }
+        TitleCanvasGroupstage1.alpha = SetAlpha1;
+        if(TitleCanvasGroupstage1.alpha>=1)
+        {
+            SetAlpha2 += PlusTime * Time.deltaTime;
+        }
+        TitleCanvasGroupstage2.alpha = SetAlpha2;
+    }
+}
