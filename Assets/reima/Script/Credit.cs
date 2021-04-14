153b229184ad8d545fb176b31147ccea82a6da24
reima398 <reima398@gmail.com>
2021-04-14 15:26:21 +0900

    クレジットに仮で文字置き

--- Assets/reima/Script/Credit.cs
+++ Assets/reima/Script/Credit.cs
@@ -9,30 +9,34 @@ public class Credit : MonoBehaviour
     GameObject itemmanage;
     [SerializeField]
     private float TextScrollSpeed = 30;
-    //　テキストの制限位置
-    [SerializeField]
-    private float LimitPosition = 2000f;
     //　エンドロールが終了したかどうか
     private bool StopEndRoll;
 
     GameObject SceneManage;
 
+
     public float WaitTime = 0;
+    //　テキストの制限位置
     public float UpPoint;
 
     public float SetAlpha = 0;
     // Start is called before the first frame update
     void Start()
     {
-        UpPoint = 1080 / 4;
+        UpPoint = 1080 / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Title");
+        }
+
         WaitTime += Time.deltaTime;
         //　エンドロール用テキストがリミットを越えるまで動かす
-        if (transform.position.y <= LimitPosition)
+        if (transform.position.y <= UpPoint)
         {
             transform.position = new Vector2(transform.position.x, transform.position.y + TextScrollSpeed * Time.deltaTime);
         }
@@ -44,13 +48,18 @@ public class Credit : MonoBehaviour
 
         text.text =
                                 "                                       \n" +
-                                "獲得したもの\n" +
-                                "                                      \n ";
-
-        //TitleBack.SetActive(true);
-        //if (TitleBack.transform.position.y < UpPoint)
-        //{
-        //    TitleBack.transform.position = new Vector3(TitleBack.transform.position.x, TitleBack.transform.position.y + 1, TitleBack.transform.position.z);
-        //}
+                                "リーダー　：　(名前入力)\n" +
+                                "                                      \n " +
+                                "副リーダー　：　(名前入力)\n" +
+                                "                                      \n " +
+                                "プランナー　：　(名前入力)\n" +
+                                "                                      \n " +
+                                "プログラマー　：　(名前入力)\n" +
+                                "                                      \n " +
+                                "グラフィッカー　：　(名前入力)\n" +
+                                "                                      \n " +
+                                "                                      \n " +
+                                "                                      \n " +
+                                "ESCで戻る\n";
     }
 }
