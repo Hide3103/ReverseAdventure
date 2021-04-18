74ccd3c8a7d6790c07692594ca5f6050c71f6f5f
reima398 <reima398@gmail.com>
2021-04-18 17:09:56 +0900

    転がる岩tp揺れて落ちる床を追加

---
+++ Assets/reima/Script/BrokeFloor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokeFloor : MonoBehaviour
+{
+    float BrokeTime = 0.0f;
+    float SetBrokeTime = 0;
+
+    float PosX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PosX = this.gameObject.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(BrokeTime);
+        if (BrokeTime > 5)
+        {
+            this.gameObject.transform.position += new Vector3(0, -1 * Time.deltaTime, 0);
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            BrokeTime += Time.deltaTime;
+            if(BrokeTime>0)
+            {
+                this.transform.position = new Vector3(PosX + Mathf.PingPong(Time.time, 0.1f), this.transform.position.y, this.transform.position.z);
+            }
+        }
+    }
+}
---
+++ Assets/reima/Script/Rock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    bool DamageOn = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += new Vector3( -1 * Time.deltaTime,0, 0);
+        this.transform.Rotate(new Vector3(0, 0, +5));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.name == "Player")
+        {
+            if (DamageOn)
+            {
+                PlayerScript.m_PlayerHp -= 1;
+                DamageOn = false;
+            }
+        }
+    }
+}
