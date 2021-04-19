45d5f16852be960c4411bfaead0cc47aa71e82a4
reima398 <reima398@gmail.com>
2021-04-19 14:35:03 +0900

    矢と発射装置の追加

---
+++ Assets/reima/Script/Arrow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+
+    private int ArrowSpeed = 3;
+    private bool DamageOn = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.position += new Vector3(-ArrowSpeed * Time.deltaTime, 0, 0);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.name == "Player")
+        {
+            if (DamageOn)
+            {
+                PlayerScript.m_PlayerHp -= 1;
+                DamageOn = false;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/reima/Script/FiringBox.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiringBox : MonoBehaviour
+{ 
+    public GameObject AroowPrehub;
+    float IntervalTime = 0;
+    bool Go = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IntervalTime += Time.deltaTime;
+        if (3 < IntervalTime)
+        {
+            if (Go)
+            {
+                Instantiate(AroowPrehub, new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z), Quaternion.identity);
+                IntervalTime = 0;
+                Go = false;
+            }
+            if(Go==false)
+            {
+                if(IntervalTime>3)
+                {
+                    Go = true;
+                }
+            }
+        }
+    }
+}
