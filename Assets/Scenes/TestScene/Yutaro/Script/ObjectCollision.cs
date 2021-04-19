e601803ecd6f947bd6b1f18c58bad7838fac1921
Ytarou2218 <zeal19300172@c-league.jp>
2021-04-19 12:03:55 +0900

    3秒後に消える足場

---
+++ Assets/Scenes/TestScene/Yutaro/Script/ObjectCollision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollision : MonoBehaviour
+{
+    public float boundHeight; //このオブジェクトを踏んだ時にはねる高さ
+
+    public bool playerStepOn = false;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if(playerStepOn == false)
+        {
+            Debug.Log("c");
+        }
+        else
+        {
+            Debug.Log("d");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            playerStepOn = true;
+            Debug.Log("b");
+        }
+        else
+        {
+            Debug.Log("a");
+        }
+    }
+}
