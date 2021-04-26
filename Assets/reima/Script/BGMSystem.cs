8ffeb03d170b45475e11999418a06cf695ad0096
reima398 <reima398@gmail.com>
2021-04-26 16:02:20 +0900

    BGMシステム途中

---
+++ Assets/reima/Script/BGMSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMSystem : MonoBehaviour
+{ 
+    private AudioSource audioSource;
+    private AudioSource audioSource2;
+
+    public AudioClip ResultSound;
+    public AudioClip Stgae1Sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+        audioSource.clip = ResultSound;
+
+        audioSource2 = this.gameObject.GetComponent<AudioSource>();
+        audioSource2.clip = Stgae1Sound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (GameSystem.IsGoal == true)
+        {
+            audioSource.Play();
+            audioSource2.Stop();
+        }
+        else
+        {
+            audioSource.Stop();
+            audioSource2.Play();
+        }
+    }
+}
