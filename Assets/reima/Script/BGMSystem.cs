483cd76292dc8e7c0620524c3ef2967e9bebaba9
reima398 <reima398@gmail.com>
2021-04-26 17:09:23 +0900

    BGMが変わるように変更

--- Assets/reima/Script/BGMSystem.cs
+++ Assets/reima/Script/BGMSystem.cs
@@ -3,35 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BGMSystem : MonoBehaviour
-{ 
-    private AudioSource audioSource;
-    private AudioSource audioSource2;
+{
+    public AudioClip sound;
 
-    public AudioClip ResultSound;
-    public AudioClip Stgae1Sound;
+    AudioSource audioSource;
+
+    bool SoundOn = true;
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = this.gameObject.GetComponent<AudioSource>();
-        audioSource.clip = ResultSound;
-
-        audioSource2 = this.gameObject.GetComponent<AudioSource>();
-        audioSource2.clip = Stgae1Sound;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (GameSystem.IsGoal==true&&SoundOn)
+        {
+            audioSource.PlayOneShot(sound);
 
-        if (GameSystem.IsGoal == true)
-        {
-            audioSource.Play();
-            audioSource2.Stop();
-        }
-        else
-        {
-            audioSource.Stop();
-            audioSource2.Play();
+            SoundOn = false;
         }
     }
 }
