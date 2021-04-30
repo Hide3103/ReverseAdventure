96572dfc78f3a34b9bbe8015d6c7eaef82f34dcd
reima398 <reima398@gmail.com>
2021-04-30 09:40:19 +0900

    パッドスクリプトの改良 ダメージエフェクトの追加

---
+++ Assets/reima/Script/DamageEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageEffect : MonoBehaviour
+{
+    Image Img;
+    public static bool DamageEffectOn = true;
+
+    void Start()
+    {
+        Img = GetComponent<Image>();
+        Img.color = Color.clear;
+    }
+
+    void Update()
+    {
+        if (DamageEffectOn)
+        {
+            this.Img.color = new Color(0.5f, 0f, 0f, 0.5f);
+            DamageEffectOn = false;
+        }
+        else
+        {
+            this.Img.color = Color.Lerp(this.Img.color, Color.clear, Time.deltaTime);
+        }
+    }
+}
--- Assets/reima/Script/PadScript.cs
+++ Assets/reima/Script/PadScript.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PadScript : MonoBehaviour
 {
+    public GameObject PadUI;
     public static bool PadOn = false;
     // Start is called before the first frame update
     void Start()
@@ -14,13 +16,25 @@ public class PadScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        CheckInput();
+        if(PadOn ==true)
+        {
+            PadUI.SetActive(true);
+        }
+    }
+
+    //接続されているコントローラーチェック
+    void CheckInput()
+    {
         // 接続されているコントローラの名前を調べる
         var ControllerNames = Input.GetJoystickNames();
 
-        // 一台もコントローラが接続されていなければエラー
         if (ControllerNames[0] == "")
         {
             PadOn = true;
         }
     }
+
+    //
 }
