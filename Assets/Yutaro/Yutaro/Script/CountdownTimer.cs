9ca988d9123aeb049d51443d7389411d06ef2741
Ytarou2218 <zeal19300172@c-league.jp>
2021-05-07 13:54:00 +0900

    タイマー、ステージ背景の差し替え

---
+++ Assets/Yutaro/Yutaro/Script/CountdownTimer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public Text timerText;
+
+    public float totalTime;
+    int seconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalTime -= Time.deltaTime;
+        seconds = (int)totalTime;
+        timerText.text = seconds.ToString();
+
+        if(totalTime <= 0f)
+        {
+            SceneManager.LoadScene("GameOver");
+            Debug.Log("制限時間終了");
+        }
+    }
+}
