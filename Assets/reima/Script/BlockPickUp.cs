2bda9bb0cc9113f73f53dd1db8afa8453ed20c13
reima398 <reima398@gmail.com>
2021-05-11 13:36:42 +0900

    ブロックを持ち上げるプログラム完成

--- Assets/reima/Script/BlockPickUp.cs
+++ Assets/reima/Script/BlockPickUp.cs
@@ -7,16 +7,19 @@ public class BlockPickUp : MonoBehaviour
     private GameObject Player;
     private GameObject SetBlockPos;
     private bool Have = false;
-    private float BlockSetPos = 1.0f;
+    private float BlockSetPos = 0.2f;
     bool Set;
     bool Flg_Throw;
+    float WaitTime = 0;
+    float SetWaitTime = 0.5f;
+    float ThrowPowwer = 200;
     // Start is called before the first frame update
     void Start()
     {
-        Player = GameObject.Find("Player");
+        Player = GameObject.Find("MotionPlayer");
         SetBlockPos = GameObject.Find("Player/BlockPos");
         var rb = this.transform.gameObject.GetComponent<Rigidbody2D>();
-        if(Have ==false)
+        if (Have == false)
         {
             rb.simulated = true;
             rb.isKinematic = true;
@@ -26,36 +29,46 @@ public class BlockPickUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(WaitTime>0)
+        {
+            WaitTime -= Time.deltaTime;
+        }
         var rb = this.transform.gameObject.GetComponent<Rigidbody2D>();
         if (Have)
         {
-            rb.freezeRotation = true;
             this.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + BlockSetPos, Player.transform.position.z);
-            if(Input.GetKeyDown(KeyCode.B) && Have == true|| Input.GetKeyDown("joystick button 2") && Have == true)
+            if ((Input.GetKeyDown(KeyCode.V)) && Have &&WaitTime<=0|| Input.GetKeyDown("joystick button 2") && Have == true && WaitTime <= 0)//X
             {
-                Flg_Throw = true;
-            }
-        }
-        if((Input.GetKeyDown(KeyCode.V))&&Have&&Flg_Throw|| Input.GetKeyDown("joystick button 2") && Have == true&&Flg_Throw)//X
-        {
-            rb.isKinematic = false;
-            rb.velocity = new Vector2(0, 0);
-            rb.AddForce(new Vector3(100,0,0));
-            Have = false;
-            Flg_Throw = false;
 
-            //this.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + BlockSetPos, Player.transform.position.z)
+                rb.isKinematic = false;
+                rb.velocity = new Vector2(0, 0);
+                Have = false;
+                Flg_Throw = false;
+
+                if (Player.transform.localScale.x > 0)
+                {
+                    rb.AddForce(new Vector3(ThrowPowwer, 0, 0));
+                }
+                if (Player.transform.localScale.x < 0)
+                {
+                    rb.AddForce(new Vector3(-ThrowPowwer, 0, 0));
+                }
+
+                //this.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + BlockSetPos, Player.transform.position.z)
+            }
         }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "Player")
+        var rb = this.transform.gameObject.GetComponent<Rigidbody2D>();
+        if (collision.gameObject.tag == "Player")
         {
-            if (Input.GetKeyDown(KeyCode.B)&&Have ==false || Input.GetKeyDown("joystick button 2") && Have == false)
+            if (Input.GetKeyDown(KeyCode.V) && Have == false || Input.GetKeyDown("joystick button 2") && Have == false)
             {
-
                 Have = true;
+                rb.freezeRotation = true;
+                WaitTime += SetWaitTime;
             }
         }
     }
