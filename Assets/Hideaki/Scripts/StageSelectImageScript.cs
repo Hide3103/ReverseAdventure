8b47c0387da00890f5eba53911ce98be2985fc5a
Hide3103 <zeal19300216@c-league.jp>
2021-05-17 14:00:01 +0900

    ステ―ジセレクトのバグ修正、クリア状況・宝石獲得数でステージが開放、Blockのスケール修正

--- Assets/Hideaki/Scripts/StageSelectImageScript.cs
+++ Assets/Hideaki/Scripts/StageSelectImageScript.cs
@@ -9,12 +9,20 @@ public class StageSelectImageScript : MonoBehaviour
     public GameObject CameraObject;
     StageSelectCameraScript cameraScript;
 
+    public GameObject TapeObject;
+    //public GameObject TapeText;
+
     Vector3 firstScale;
 
     public Sprite LockImage = null;
     public Sprite OpenImage = null;
 
     SpriteRenderer spriteRenderer;
+
+    public Canvas m_canvas;
+
+    public Text StageText;
+    RectTransform rectTrans;
 
     // Start is called before the first frame update
     void Start()
@@ -23,23 +31,27 @@ public class StageSelectImageScript : MonoBehaviour
         firstScale = transform.localScale;
 
         spriteRenderer = this.GetComponent<SpriteRenderer>();
-
-        if (ThisStageNum != 0)
-        {
-            if (GameSystem.GetStageCleared(ThisStageNum) == false)
-            {
-                spriteRenderer.sprite = LockImage;
-            }
-            else
-            {
-                spriteRenderer.sprite = OpenImage;
-            }
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (ThisStageNum != 0)
+        {
+            if (GameSystem.GetStageCleared(ThisStageNum) == true
+                        && GameSystem.ClearJuwel[ThisStageNum - 1] <= GameSystem.GetAllStageJuwelNum())
+            {
+                TapeObject.SetActive(false);
+                //TapeText.SetActive(false);
+                spriteRenderer.sprite = OpenImage;
+            }
+            else
+            {
+                spriteRenderer.sprite = LockImage;
+                StageText.text = ThisStageNum + "ステージクリア\n" + GameSystem.GetClearJuwelNum(ThisStageNum - 1) + "個所持";
+            }
+        }
+
         if (StageSelectCameraScript.SelectingStageNum == ThisStageNum)
         {
             this.transform.localScale = firstScale + new Vector3(0.1f, 0.1f, 0.1f) * Mathf.Abs(Mathf.Sin(Time.time * 2));
@@ -48,10 +60,19 @@ public class StageSelectImageScript : MonoBehaviour
         {
             this.transform.localScale = firstScale;
         }
+    }
 
-        //if(GameSystem.GetStageCleared(cameraScript.) == )
-        //{
+    private Vector3 GetWorldPositionFromRectPosition(RectTransform rect)
+    {
+        //UI座標からスクリーン座標に変換
+        Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(m_canvas.worldCamera, rect.position);
 
-        //}
+        //ワールド座標
+        Vector3 result = Vector3.zero;
+
+        //スクリーン座標→ワールド座標に変換
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(rect, screenPos, m_canvas.worldCamera, out result);
+
+        return result;
     }
 }
