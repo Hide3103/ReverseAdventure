d8f940625962205ec433feb2dc61cb43d76e864b
reima398 <reima398@gmail.com>
2021-05-28 14:36:35 +0900

    gameover時に音が変わるように変更

--- Assets/reima/Script/Stage1BGM.cs
+++ Assets/reima/Script/Stage1BGM.cs
@@ -13,7 +13,7 @@ public class Stage1BGM : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(GameSystem.IsGoal)
+        if(GameSystem.IsGoal||MotionPlayer.m_PlayerHp==0)
         {
             Destroy(this.gameObject);
         }
